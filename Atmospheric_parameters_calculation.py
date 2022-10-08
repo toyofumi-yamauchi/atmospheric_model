64e7d9a47102b1a03cd09c7bfbd1dc899479ce59
Toyo <ty20@illinois.edu>
2022-10-08 18:31:58 -0500

    Update

--- Atmospheric_parameters_calculation.py
+++ Atmospheric_parameters_calculation.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 from functions import load_NRLMSIS_data, P_from_n_and_T, neutral_flux
 
-R_a = 8.314 # universal gas constant [J/mol/K]
+R   = 8.314 # universal gas constant [J/mol/K]
 k_B = 1.380649e-23 # Boltzmann constant [J/K]
 N_A = 6.022e23 # Avogadro's number [#/mole]
 G = 6.674e-11 # Gravitational constant [m^3/kg/s^2]
@@ -30,8 +30,11 @@ n_total = n_N2 + n_O2 + n_N + n_O + n_Ar + n_He + n_H
 # total mass density [kg/m^-3]
 m_total = (n_N2*28 + n_O2*32 + n_N*14 + n_O*16 + n_Ar*40 + n_He*2 + n_H*1)/1000/N_A
 
-# average "air" molar mass [kg/mole]
+# average "air" molar mass [kg/mol]
 M_ave   = (n_N2*28 + n_O2*32 + n_N*14 + n_O*16 + n_Ar*40 + n_He*2 + n_H*1)/n_total/1000
+
+# average "air" mass [kg]
+m_air   = M_ave/N_A
 
 # atmospheric parameter
 A_sc = 1.0 # cross-sectional area [m^2]
@@ -43,25 +46,29 @@ V_dot = np.zeros(len(h)) # volumetric flow rate [sccm]
 for i in range(0,len(h)):
     P[i] = P_from_n_and_T(n_total[i],T[i])
     Γ[i], v_sc[i] = neutral_flux(h[i],n_total[i])
-    m_dot[i] = A_sc*Γ[i]*M_ave[i] # [kg/s]
-    V_dot[i] = m_dot[i]*R_a*300/(M_ave[i]*N_A)/101325*60*1e6 # [sccm]
+    m_dot[i] = m_air[i]*Γ[i]*A_sc
+    V_dot[i] = m_dot[i]/m_air[i]/N_A*(R*300/101325)*60*1e6
 
 # plots
 figure_size = (4.75,6.33)
 
 fig, ax = plt.subplots(figsize=figure_size)
-ax.semilogy(h,m_dot/A_sc)
+l1 = ax.semilogy(h,m_dot/A_sc,label='$\dot{m}$')
 ax.set_xlim(50,400)
 ax.set_xlabel('Altitude, km')
 ax.set_ylim(1e-8,1e1)
 ax.set_yticks(np.logspace(-8,1,10))
 ax.set_ylabel('Mass flow rate/A, kg/s/$m^2$')
 ax2 = ax.twinx()
-ax2.semilogy(h,V_dot)
-ax2.set_ylim(1e-1,1e8)
-ax2.set_yticks(np.logspace(-1,8,10))
-ax2.set_ylabel('Mass flow rate/A, sccm/$m^2$')
+l2 = ax2.semilogy(h,V_dot,'--',label='$\dot{V}$')
+ax2.set_ylim(1e0,1e9)
+ax2.set_yticks(np.logspace(0,9,10))
+ax2.set_ylabel('Volmetric flow rate/A, sccm/$m^2$')
 ax.grid()
+ax.set_title('Flow rate vs Altitude')
+lns = l1+l2
+labs = [l.get_label() for l in lns]
+ax.legend(lns,labs,framealpha=1.0)
 fig.tight_layout()
 fig.savefig('Mass flow rate vs Altitude',dpi=300)
 plt.show()
@@ -81,7 +88,8 @@ ax.set_ylim(1e14,1e23)
 ax.set_yticks(np.logspace(14,23,10))
 ax.set_ylabel('Number density, m$^-$$^3$')
 ax.grid()
-ax.legend()
+ax.legend(framealpha=1.0)
+ax.set_title('Density vs Altitude')
 fig.tight_layout()
 fig.savefig('Density vs Altitude',dpi=300)
 plt.show()
@@ -99,15 +107,16 @@ ax2.set_ylim(1e-7*760/101325,1e2*760/101325)
 ax2.set_yticks(np.logspace(-9,0,10))
 ax2.set_ylabel('Pressure, Torr')
 ax.grid()
+ax.set_title('Pressure vs Altitude')
 fig.tight_layout()
 fig.savefig('Pressure vs Altitude',dpi=300)
 plt.show()
-#%%
+#%
 x = 10
-print('h    = {} km'.format(h[x]))
-print('v_sc = {:.2f} m/s'.format(v_sc[x]))
-print('T    = {:.1f} K'.format(T[x]))
-print('P    = {:.1e} Pa'.format(P[x]))
+print('h       = {} km'.format(h[x]))
+print('v_sc    = {:.2f} m/s'.format(v_sc[x]))
+print('T       = {:.1f} K'.format(T[x]))
+print('P       = {:.1e} Pa'.format(P[x]))
 print('n_total = {:.2e} m^-3'.format(n_total[x]))
 print('n_N2    = {:.2e} m^-3 ({:.1f}%)'.format(n_N2[x],n_N2[x]/n_total[x]*100))
 print('n_O2    = {:.2e} m^-3 ({:.1f}%)'.format(n_O2[x],n_O2[x]/n_total[x]*100))
@@ -116,4 +125,5 @@ print('n_O     = {:.2e} m^-3 ({:.1f}%)'.format(n_O[x],n_O[x]/n_total[x]*100))
 print('n_Ar    = {:.2e} m^-3 ({:.1f}%)'.format(n_Ar[x],n_Ar[x]/n_total[x]*100))
 print('n_He    = {:.2e} m^-3 ({:.1f}%)'.format(n_He[x],n_He[x]/n_total[x]*100))
 print('n_H     = {:.2e} m^-3 ({:.1f}%)'.format(n_H[x],n_H[x]/n_total[x]*100))
-print('V_dot = {:.2e} sccm'.format(V_dot[x]))
+print('m_dot/A = {:.2e} kg/s/m^2'.format(m_dot[x]))
+print('V_dot/A = {:.2e} sccm/m^2'.format(V_dot[x]))
